89dae3705cb42409092f5662e312d1b670a56250
Aniket Jaiswal <aniketjaiswal6297@gmail.com>
2024-09-23 18:43:45 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// console.log("Sita Ram");
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+app.get('/ani',(req,res)=>{
+         res.send("Sita Ram");
+})
+app.get('/ket',(req,res)=>{
+    res.send('<h1>Sita Ram Ram Ram</h1>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
